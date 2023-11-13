be2421404523cbc231b32915cea069b3baa5eb1c
Zulan <roi.zulan@gmail.com>
2023-11-13 17:46:09 +0800

    backend setup

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 from . views import Endpoints
+from . views import MyTokenObtainPairView
 
 urlpatterns = [
-    path('', Endpoints.as_view(), name='test_view')
+    path('', Endpoints.as_view(), name='test_view'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from . serializers import MyTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+        
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 class Endpoints(APIView):
     def get(self, request):
         data = {
