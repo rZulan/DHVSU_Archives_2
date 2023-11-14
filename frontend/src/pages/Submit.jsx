85ff367ab90bbdf7cafc231effe1b528908ceed9
whiskey00 <119928947+whiskey00@users.noreply.github.com>
2023-11-15 07:22:14 +0800

    submit

--- frontend/src/pages/Submit.jsx
+++ frontend/src/pages/Submit.jsx
@@ -91,8 +91,32 @@ const Submit = () => {
     setFormData({
       ...formData,
       sections,
-      newSectionTitle: '',
-      newSectionContent: '',
+      newSectionTitle: "",
+      newSectionContent: "",
+    });
+
+    // Clear error message for the new section
+    setErrors({
+      ...errors,
+      sections: [...errors.sections, { title: "", content: "" }],
+    });
+  };
+
+  const removeSection = (index) => {
+    const sections = [...formData.sections];
+    sections.splice(index, 1);
+    setFormData({
+      ...formData,
+      sections,
+    });
+
+    // Remove error message for the removed section
+    setErrors({
+      ...errors,
+      sections: [
+        ...errors.sections.slice(0, index),
+        ...errors.sections.slice(index + 1),
+      ],
     });
   };
 
@@ -203,125 +227,217 @@ const Submit = () => {
   }
 
   return (
-    <div className="main-body bg-[#F6F6F6]">
-      <div className="min-h-screen">
-        <h1 className="text-3xl font-bold text-center my-4">Submit a Document</h1>
-        <div className="container mx-auto px-4 py-8">
-          <form onSubmit={handleSubmit} encType="multipart/form-data" className="mb-8">
-            <div className="mb-4">
-              <label htmlFor="title" className="block mb-2 font-semibold">
-                Title
-              </label>
-              <input
-                type="text"
-                name="title"
-                id="title"
-                value={formData.title}
-                onChange={handleChange}
-                className="border border-gray-400 rounded py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-400"
-              />
-            </div>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center mt-24">
+      <div className="bg-white p-8 rounded shadow-lg w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Submit a Document
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          encType="multipart/form-data"
+          className="mb-8"
+        >
+          <label htmlFor="title" className="block mb-2">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={formData.title}
+            onChange={handleChange}
+            className={`border border-gray-400 rounded py-2 px-4 mb-4 w-full ${
+              errors.title && "border-red-500"
+            }`}
+          />
+          {errors.title && (
+            <p className="text-red-500 text-sm">{errors.title}</p>
+          )}
 
-            <div className="mb-4">
-              <label htmlFor="abstract" className="block mb-2 font-semibold">
-                Abstract
-              </label>
-              <textarea
-                name="abstract"
-                id="abstract"
-                value={formData.abstract}
-                onChange={handleChange}
-                className="border border-gray-400 rounded py-2 px-4 w-full h-32 focus:outline-none focus:ring focus:border-blue-400"
-              />
-            </div>
+          <label htmlFor="abstract" className="block mb-2">
+            Abstract
+          </label>
+          <textarea
+            name="abstract"
+            id="abstract"
+            value={formData.abstract}
+            onChange={handleChange}
+            className={`border border-gray-400 rounded py-2 px-4 mb-4 w-full h-32 ${
+              errors.abstract && "border-red-500"
+            }`}
+          />
+          {errors.abstract && (
+            <p className="text-red-500 text-sm">{errors.abstract}</p>
+          )}
 
-            <div className="mb-4">
-              <label htmlFor="file" className="block mb-2 font-semibold">
-                File
-              </label>
-              <input
-                type="file"
-                name="file"
-                id="file"
-                onChange={(e) => setFormData({ ...formData, file: e.target.files[0] })}
-                className="mb-2 focus:outline-none"
-              />
-            </div>
+          <label htmlFor="file" className="block mb-2">
+            File
+          </label>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            onChange={(e) =>
+              setFormData({ ...formData, file: e.target.files[0] })
+            }
+            className={`mb-4 ${errors.file && "border-red-500"}`}
+          />
+          {errors.file && <p className="text-red-500 text-sm">{errors.file}</p>}
 
-            <div className="mb-8">
-              <h2 className="text-lg font-semibold mb-2">Document Sections</h2>
-              {formData.sections.map((section, index) => (
-                <div key={index} className="mb-10">
-                  <input
-                    type="text"
-                    name="title"
-                    value={section.title}
-                    onChange={(e) => handleSectionChange(e, index)}
-                    placeholder="Section Title"
-                    className="border border-gray-400 rounded py-2 px-4 mb-2 w-full focus:outline-none focus:ring focus:border-blue-400"
-                  />
-                  <textarea
-                    name="content"
-                    value={section.content}
-                    onChange={(e) => handleSectionChange(e, index)}
-                    placeholder="Section Content"
-                    className="border border-gray-400 rounded py-2 px-4 w-full h-24 focus:outline-none focus:ring focus:border-blue-400"
-                  />
-                  <p className="border border-black p-2 inline-block">
-                    {`${(section.similarityScore * 100).toFixed(1)}%`}
-                  </p>
-                  <label className="text-sm ml-1">
-                    Plagiarized
-                  </label>
-                </div>
-              ))}
-              <p className="text-lg font-semibold mb-2 mt-4">
-                Overall Plagiarism Score: <u>{isNaN(averageSimilarityScore) ? '0%' : `${(averageSimilarityScore * 100).toFixed(1)}%`} Plagiarized</u>
-              </p>
-
-              <div className="flex items-start">
+          <div className="mb-8">
+            <h2 className="text-lg font-semibold mb-4">Document Sections</h2>
+            {formData.sections.map((section, index) => (
+              <div key={index} className="mb-6">
                 <input
                   type="text"
-                  value={formData.newSectionTitle}
-                  onChange={(e) => setFormData({ ...formData, newSectionTitle: e.target.value })}
-                  placeholder="New Section Title"
-                  className="border border-gray-400 rounded py-2 px-4 mr-2 w-full focus:outline-none focus:ring focus:border-blue-400"
+                  name="title"
+                  value={section.title}
+                  onChange={(e) => handleSectionChange(e, index)}
+                  placeholder="Section Title"
+                  className={`border border-gray-400 rounded py-2 px-4 mb-2 w-full ${
+                    errors.sections &&
+                    errors.sections[index] &&
+                    errors.sections[index].title &&
+                    "border-red-500"
+                  }`}
                 />
+                {errors.sections &&
+                  errors.sections[index] &&
+                  errors.sections[index].title && (
+                    <p className="text-red-500 text-sm">
+                      {errors.sections[index].title}
+                    </p>
+                  )}
                 <textarea
-                  value={formData.newSectionContent}
-                  onChange={(e) =>
-                    setFormData({ ...formData, newSectionContent: e.target.value })
-                  }
-                  placeholder="New Section Content"
-                  className="border border-gray-400 rounded py-2 px-4 w-full h-24 focus:outline-none focus:ring focus:border-blue-400"
+                  name="content"
+                  value={section.content}
+                  onChange={(e) => handleSectionChange(e, index)}
+                  placeholder="Section Content"
+                  className={`border border-gray-400 rounded py-2 px-4 w-full h-24 ${
+                    errors.sections &&
+                    errors.sections[index] &&
+                    errors.sections[index].content &&
+                    "border-red-500"
+                  }`}
                 />
-                <button
-                  type="button"
-                  onClick={addNewSection}
-                  className="bg-blue-500 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Add Section
-                </button>
+                {errors.sections &&
+                  errors.sections[index] &&
+                  errors.sections[index].content && (
+                    <p className="text-red-500 text-sm">
+                      {errors.sections[index].content}
+                    </p>
+                  )}
+                <div className="flex items-center mt-2">
+                  <p
+                    className={`border p-2 inline-block ${
+                      section.similarityScore > 0.5
+                        ? "bg-red-500 text-white"
+                        : "bg-green-500 text-white"
+                    }`}
+                  >
+                    {`${(section.similarityScore * 100).toFixed(1)}%`}
+                  </p>
+                  <label className="text-sm ml-2">
+                    {section.similarityScore > 0.5
+                      ? "Plagiarized"
+                      : "Not Plagiarized"}
+                  </label>
+                  <button
+                    type="button"
+                    onClick={() => removeSection(index)}
+                    className="ml-auto bg-red-500 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                  >
+                    Remove Section
+                  </button>
+                </div>
               </div>
-            </div>
+            ))}
 
-            <div className="flex justify-between">
+            <div className="flex items-start">
               <button
                 type="button"
-                onClick={handleVerify}
-                className="bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={addDefaultSections}
+                className="bg-gray-500 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
               >
-                Verify Plagiarism
-              </button>
-              <button
-                type="submit"
-                className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Submit Document
+                Add Default Sections
               </button>
             </div>
-          </form>
-        </div>
+            <p className="text-lg font-semibold mb-2 mt-4">
+              Overall Plagiarism Score:{" "}
+              <u>
+                {isNaN(averageSimilarityScore)
+                  ? "0%"
+                  : `${(averageSimilarityScore * 100).toFixed(1)}%`}{" "}
+                Plagiarized
+              </u>
+            </p>
+
+            {/* Display section error message */}
+            {errors.sections && errors.sections[0] && (
+              <p className="text-red-500 text-sm">{errors.sections[0].title}</p>
+            )}
+
+            <div className="flex items-start">
+              <input
+                type="text"
+                value={formData.newSectionTitle}
+                onChange={(e) =>
+                  setFormData({ ...formData, newSectionTitle: e.target.value })
+                }
+                placeholder="New Section Title"
+                className={`border border-gray-400 rounded py-2 px-4 mr-2 w-full ${
+                  errors.newSectionTitle && "border-red-500"
+                }`}
+              />
+              {errors.newSectionTitle && (
+                <p className="text-red-500 text-sm">{errors.newSectionTitle}</p>
+              )}
+              <textarea
+                value={formData.newSectionContent}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    newSectionContent: e.target.value,
+                  })
+                }
+                placeholder="New Section Content"
+                className={`border border-gray-400 rounded py-2 px-4 w-full h-24 ${
+                  errors.newSectionContent && "border-red-500"
+                }`}
+              />
+              {errors.newSectionContent && (
+                <p className="text-red-500 text-sm">
+                  {errors.newSectionContent}
+                </p>
+              )}
+              <button
+                type="button"
+                onClick={addNewSection}
+                className="bg-blue-500 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
+              >
+                Add Section
+              </button>
+            </div>
+          </div>
+          {errors.general && (
+            <p className="text-red-500 text-sm mb-4">{errors.general}</p>
+          )}
+          <div className="flex justify-between">
+            <button
+              type="button"
+              onClick={handleVerify}
+              className="bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Verify Plagiarism
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit Document
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
